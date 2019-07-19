de0cfcee007092cd077ba06ab4e6e7de22690414
MHupb <miguel.hincapiec@upb.edu.co>
2019-07-19 01:47:14 -0500

    Little changes to style

--- PersonalWeb/js/main.js
+++ PersonalWeb/js/main.js
@@ -1,8 +1,11 @@
-
 document.getElementById("YanKenPo").onclick = function () {
     location.href = "Yan Ken Po.html";
 };
 
 document.getElementById("LogoQuizGameIdk").onclick = function () {
     location.href = "LogoQuizGameIdk.html";
+};
+
+document.getElementById("Frankenstein").onclick = function () {
+    location.href = "./Simulations/JustLikeTheSimulations/Frankenstein (The Monster)/index.html";
 };
